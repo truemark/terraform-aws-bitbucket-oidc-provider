17294bd2e1abad6d191dad39622b58ee7a453ac6
Erik Jensen <ejensen@netradius.com>
2021-08-21 22:04:14 -0600

    Adding functionality to handle UUID with {] and without

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 locals {
+  workspace_uuid = "{${replace(var.workspace_uuid, "/[{}]/", "")}}"
+
   url = "https://api.bitbucket.org/2.0/workspaces/${var.workspace_name}/pipelines-config/identity/oidc"
   client_id_list = [
-    "ari:cloud:bitbucket::workspace/${var.workspace_uuid}"
+    "ari:cloud:bitbucket::workspace/${local.workspace_uuid}"
   ]
   thumbprint_list = concat(var.additional_thumbprints,
   [
